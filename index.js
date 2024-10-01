e8a1e514ed0bd01c6730ae2c68edcc52855c1749
flying-bisom <117004668+flying-bisom@users.noreply.github.com>
2024-10-01 23:46:48 +0530

    animation added

--- index.js
+++ index.js
@@ -1,52 +1,70 @@
-let allInstruments = document.querySelectorAll("button");
+var numberOfDrums=document.querySelectorAll("button").length;
+for(i=0; i<numberOfDrums;i++){
 
-var button = allInstruments.forEach((instrument) => {
-  instrument.addEventListener("click", () => {
-    let Instrument = instrument.classList.value;
-    //Audio added to each button,when it gets clicked
-    let audio = new Audio(`./sounds/${Instrument}.mp3`);
-    audio.play();
-  });
-});
-
-allInstruments.forEach((element) => {
-  element.addEventListener("keypress", (event) => {
-    switch (event.key) {
-      case "a":
-        var audio = new Audio("./sounds/tom1.mp3");
-        audio.play();
-        break;
-      case "s":
-        audio = new Audio("./sounds/tom2.mp3");
-        audio.play();
-        break;
-      case "d":
-        audio = new Audio("./sounds/tom3.mp3");
-        audio.play();
-        break;
-      case "f":
-        audio = new Audio("./sounds/tom4.mp3");
-        audio.play();
-        break;
-      case "j":
-        audio = new Audio("./sounds/snare.mp3");
-        audio.play();
-        break;
-      case "k":
-        audio = new Audio("./sounds/kick-bass.mp3");
-        audio.play();
-        break;
-      case "l":
-            audio = new Audio("./sounds/crash.mp3");
-            audio.play();
+    document.querySelectorAll("button")[i].addEventListener("click",function (){
+        var buttons= this.innerHTML;
+        makeSound(buttons);
+       buttonAnimation(buttons);
+    
+    
+    });   
+    document.addEventListener("keypress",function(event){
+       
+        makeSound(event.key);
+        buttonAnimation(event.key);
+    });
+    function makeSound(key){
+        switch (key) {
+            case "a":
+                   var tom1=new Audio("sounds/tom1.mp3");
+                   tom1.play();
             break;
-      default:
-        console.log("invalid key");
+           case "s":
+                    var tom2=new Audio("sounds/tom2.mp3");
+                    tom2.play();
+            break;
+         
+            case "d":
+                    var tom3=new Audio("sounds/tom3.mp3");
+                    tom3.play();
+            break;
+         
+            case "f":
+                    var tom4=new Audio("sounds/tom4.mp3");
+                    tom4.play();
+            break;
+         
+            case "j":
+                    var snare=new Audio("sounds/snare.mp3");
+                    snare.play();
+            break;
+            case "k":
+                var crash=new Audio("sounds/crash.mp3");
+                crash.play();
         break;
-    }
-  });
-});
+         
+            case "l":
+                    var kick=new Audio("sounds/kick-bass.mp3");
+                    kick.play();
+            break;
+            
+           
+            default: console.log(buttons);
+                break;
+         }
+           
 
-function Animation(){
-    instrument
+    }
+    function buttonAnimation(currentkey){
+      var activebutton=document.querySelector("."+currentkey);
+      activebutton.classList.add("pressed");
+     
+      setTimeout(function(){
+        activebutton.classList.remove("pressed");
+
+      },100);
+
+    }
+
 }
+
